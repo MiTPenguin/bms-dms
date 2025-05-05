53bbb276621df3a74f64a9e987044eaa02366e37
nsabell <nabell@octant.bio>
2025-05-05 10:25:16 -0700

    cmds

--- sumstats/cmds.sh
+++ sumstats/cmds.sh
@@ -3,19 +3,23 @@
 Rscript --vanilla src/model.R \
     --file mapped_counts/TYK2-run1.mapped-counts.tsv \
     --outfile sumstats/TYK2-run1.sumstats.tsv \
-    --nworkers 35
+    --nworkers 35 \
+    --model global
 
 Rscript --vanilla src/model.R \
     --file mapped_counts/TYK2-run2.mapped-counts.tsv \
     --outfile sumstats/TYK2-run2.sumstats.tsv \
-    --nworkers 35
+    --nworkers 35 \
+    --model global
 
 Rscript --vanilla src/model.R \
     --file mapped_counts/TYK2-run3.mapped-counts.tsv \
     --outfile sumstats/TYK2-run3.sumstats.tsv \
-    --nworkers 35
+    --nworkers 35 \
+    --model global
 
 Rscript --vanilla src/model.R \
     --file mapped_counts/TYK2-vamp2.mapped-counts.tsv\
     --outfile sumstats/TYK2-vamp2.sumstats.tsv \
-    --nworkers 35
+    --nworkers 35 \
+    --model flow
